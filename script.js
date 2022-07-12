4903a86014073147438983e732e181d99cf67c4e
Oseias Fiorindo Bonetti <107428464+Oseias123@users.noreply.github.com>
2022-07-12 17:00:06 -0300

    .....

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelectodocument.querySelector('.icon').src = "imagens/fechar.png";r('.icon').src = "imagens/Menu.png"
+    }  
+    else {
+        
+    }
+        menuMobile.classList.add('open');
+       
+    }
+  
